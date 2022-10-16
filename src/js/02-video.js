99f2b7f64bbc645aa6a66d560c7c0becbe3e9c3f
Yurii Zahrai <66515294+yuriizahrai@users.noreply.github.com>
2022-10-16 18:54:17 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,6 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
- playerCurrentTime = 0;
-
       const iframe = document.querySelector('iframe');
       const player = new Player(iframe);
 
